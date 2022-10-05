d38a1bcc7f2bc70cce276bdfd63789ecbc3d359c
Mouhamed Lamine TALL <mouhamedlamine.tall1@orange-sonatel.com>
2022-10-05 09:31:48 +0000

    Initial Commit

---
+++ src/test/java/sn/sonatel/springjenkinscicd/SpringJenkinsCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.sonatel.springjenkinscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
